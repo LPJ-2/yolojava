571899fd83302a6bcab07f840c23d223a1592890
LPJ-2 <igor_krat@mail.ru>
2016-03-28 22:06:57 +0300

    user input

--- calculate.java
+++ calculate.java
@@ -1,5 +1,14 @@
 public class calculate {
 	public static void main(String[] arg){
+
         System.out.println("calculate...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        int sqr = first * second;
+        int minus = first - second;
+            System.out.println("Sum "+ summ);
+            System.out.println("sqr "+ sqr);
+            System.out.println("minus "+ minus);
     }
 }
